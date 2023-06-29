9d86f2f0cbba82a52a0d9312e3029c7e00f6028b
Ana Toader <Ana.Toader@softwire.com>
2023-06-29 14:14:08 +0300

    Reverted to initial state

--- src/fibonacci.ts
+++ src/fibonacci.ts
@@ -1,57 +1,17 @@
-export const computeFibonacciNumber = (position: number | null, isRecursive: boolean = false): number => {
-    if (isRecursive) return recursiveFibonacci(position);
+export const computeFibonacciNumber = (position: number): number => {
+    let i = 1;
+    let j = 1;
 
-    let notNullPosition = position;
-    if (notNullPosition === null) {
-        notNullPosition = 1;   
-    }
-
-    if (position === 0) {
-        return 0;
-    }
-    if (position < 0) {
-        return computeNegativeFibonacci(position);
-    }
-
-    if (notNullPosition <= 2) {
+    if (position <= 2) {
         return 1;
     }
 
-    let smallFibonacciNumber = 1;
-    let largeFibonacciNumber = 1;
-
     let currentPosition = 2;
-    while (currentPosition < notNullPosition) {
-        const nextFibonacciNumber = smallFibonacciNumber + largeFibonacciNumber;
-        smallFibonacciNumber = largeFibonacciNumber;
-        largeFibonacciNumber = nextFibonacciNumber;
+    while (currentPosition < position) {
+        const temp = i;
+        i = j;
+        j += temp;
         currentPosition++;
     }
-
-    return largeFibonacciNumber;
+    return j;
 };
-
-const recursiveFibonacci = (initialPosition: number, left: number = 0, right: number = 1, position?: number): number => {
-    const currentPosition = position ?? initialPosition;
-    if (initialPosition === 0) return 0;
-    if (currentPosition === 0) return left;
-    if (initialPosition > 0) {
-        return recursiveFibonacci(initialPosition, right, left + right, currentPosition - 1);
-    } else {
-        return recursiveFibonacci(initialPosition, right - left, left, currentPosition + 1);
-    }
-}
-
-export const computeFibonacciArray = (start: number, endInclusive: number): number[] => {
-    const inputArray = [...Array(endInclusive - start + 1).keys()].map(i => i + start);
-    return inputArray.map(x => computeFibonacciNumber(x));
-}
-
-const computeNegativeFibonacci = (position: number): number => {
-    if (position >= 0) {
-        throw new Error(`Position must be less than zero! Received: ${position}.`);
-    }
-    const resultIsNegative = position % 2 === 0;
-    const absoluteResult = computeFibonacciNumber(-position);
-    return resultIsNegative ? absoluteResult * -1 : absoluteResult;
-}
